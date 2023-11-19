e32cc055906693160907cb1c3e8ece7f1eb95622
zkac393 <michal.romero.2021@live.rhul.ac.uk>
2023-11-19 20:45:28 +0000

    Implemented player health.
    Added code that allows the player to be damaged and healed. In addition I have implemented
    an animated health bar that represents the players health - with indications if
    they got damaged or healed.

---
+++ Final Year Project/Assets/Scripts/Player/PlayerHP.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour
+{
+    private float health; // Player health
+    private float maxHealth = 100f; // Player max health
+    private float lerpTimer;
+    private float delaySpeed = 4f; // Speed for health bar change
+    public Image healthBar;
+    public Image backHealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth; // Initialise health to max at start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);// Ensure health stays within bounds
+        UpdateHealth();
+
+        // USED AS TESTING - Check for key presses to simulate damage or healing 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Heal(Random.Range(5, 10));
+        }
+    }
+
+    // Updates the health bar UI to reflect current health
+    public void UpdateHealth()
+    {
+        Debug.Log(health);
+        float fill = healthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float healthFraction = health / maxHealth;
+
+        // Math behind player getting damaged
+        if (fillBack > healthFraction)
+        {
+            healthBar.fillAmount = healthFraction;
+            backHealthBar.color = Color.red; // Change color to red to indicate damage
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / delaySpeed; // Calculate interpolation progress
+            percentComplete = percentComplete * percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, healthFraction, percentComplete);
+        }
+
+        // Math behind player getting healed
+        if (fill < healthFraction)
+        {
+            backHealthBar.fillAmount = healthFraction;
+            backHealthBar.color = Color.green; // Change color to magenta to indicate healing
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / delaySpeed; // Calculate interpolation progress
+            percentComplete = percentComplete * percentComplete;
+            healthBar.fillAmount = Mathf.Lerp(fill, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    // Reduces player's health
+    public void TakeDamage (float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+
+    // Increases player's health
+    public void Heal(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+}
