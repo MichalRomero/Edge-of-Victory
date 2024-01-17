c2d90bba59613e0b0c59aee2163b4ff706590866
zkac393 <michal.romero.2021@live.rhul.ac.uk>
2024-01-17 16:47:00 +0000

    Added a 'PlayerVisable' method Added a 'PlayerVisable' method which shoots a ray and follows the player when they are within view distance.

--- Final Year Project/Assets/Scripts/Enemy/Enemy.cs
+++ Final Year Project/Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,12 +12,16 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private string currentState;
     public Path path;
+    private GameObject player;
+    public float viewDistance = 20f;
+    public float fov = 85f;
     // Start is called before the first frame update
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
         
     }
 
@@ -24,6 +29,7 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         Debug.Log("Enemy hp: " + currentHealth);
+        PlayerVisable();
     }
 
     int currentHealth;
@@ -32,6 +38,27 @@ public class Enemy : MonoBehaviour
     void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+
+    // Check if player is within range and within FOV of enemy
+    public bool PlayerVisable()
+    {
+        if (player != null)
+        {
+            // Is the player close enough to be soon?
+            if (Vector3.Distance(transform.position, player.transform.position) < viewDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position;
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if (angleToPlayer >= -fov && angleToPlayer <= fov)
+                {
+                    Ray ray = new Ray(transform.position, targetDirection);
+                    Debug.DrawRay(ray.origin, ray.direction * viewDistance);
+                }
+            }
+        }
+        return true;
     }
 
     public void TakeDamage(int amount)
